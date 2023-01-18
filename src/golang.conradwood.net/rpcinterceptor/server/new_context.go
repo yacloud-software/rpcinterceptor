d2abe09bca8373efd01f7aa7e969aa17346eaecb
Conrad Wood <cnw@conradwood.net>
2023-01-18 17:34:48 +0000

    more compatibility fixes
    Change-Id: I7586e1bd7ad11b85a7637c9b28644cdbc3a18134

--- src/golang.conradwood.net/rpcinterceptor/server/new_context.go
+++ src/golang.conradwood.net/rpcinterceptor/server/new_context.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"context"
-	ic "golang.conradwood.net/apis/rpcinterceptor"
-	//	"golang.conradwood.net/go-easyops/auth"
+	"flag"
 	"fmt"
+	apb "golang.conradwood.net/apis/auth"
+	ic "golang.conradwood.net/apis/rpcinterceptor"
+	"golang.conradwood.net/go-easyops/auth"
+	"golang.conradwood.net/go-easyops/authremote"
+	"golang.conradwood.net/go-easyops/cache"
 	"golang.conradwood.net/go-easyops/common"
 	"golang.conradwood.net/go-easyops/ctx"
+	"time"
+)
+
+var (
+	print_callers = flag.Bool("print_callers", false, "if true print callers from request(payload)")
+	token_cache   = cache.New("token_cache", time.Duration(60)*time.Minute, 10000)
 )
 
 func deprecate_rpc_interceptor(ictx context.Context, req *ic.InterceptRPCRequest) (*ic.InterceptRPCResponse, error) {
 	ls := ctx.GetLocalState(ictx)
+	if *print_callers || *debug {
+		fmt.Printf("Intercept from %s/%s\n", req.Service, req.Method)
+	}
 	if *debug {
 		fmt.Printf("Request: %#v\n", req)
-		fmt.Printf("Request.Meta: %#v\n", req.InMetadata)
-		fmt.Printf("Context: %s\n", ictx)
+		m := req.InMetadata
+		if m != nil {
+			u := auth.UserIDString(m.User)
+			s := auth.UserIDString(m.Service)
+			fmt.Printf("Request.Meta: ServiceToken: %s, (user:%s,service:%s)\n", m.ServiceToken, u, s)
+		}
+		//fmt.Printf("Context: %s\n", ictx)
 		fmt.Printf("Creating response from context %s\n", ctx.Context2String(ictx))
 	}
 	res := &ic.InterceptRPCResponse{
@@ -28,6 +46,8 @@ func deprecate_rpc_interceptor(ictx context.Context, req *ic.InterceptRPCRequest
 		SignedCallerService: ls.CallingService(),
 		SignedCallerUser:    ls.User(),
 	}
+
+	// if we do not have the information, try from context.metadata
 	mt := req.InMetadata
 	if mt != nil {
 		if res.SignedCallerService == nil {
@@ -38,7 +58,24 @@ func deprecate_rpc_interceptor(ictx context.Context, req *ic.InterceptRPCRequest
 			res.SignedCallerUser = mt.SignedUser
 			res.CallerUser = common.VerifySignedUser(res.SignedCallerUser)
 		}
+
+		// if we still do not have the information, try from context.metadata.servicetoken
+		if res.SignedCallerService == nil && mt.ServiceToken != "" {
+			csu := token_cache.Get(mt.ServiceToken)
+			var su *apb.SignedUser
+			if csu != nil {
+				su = csu.(*apb.SignedUser)
+			} else {
+				su = authremote.SignedGetByToken(ictx, mt.ServiceToken)
+				token_cache.Put(mt.ServiceToken, su)
+			}
+			res.SignedCallerService = su
+			res.CallerService = common.VerifySignedUser(res.SignedCallerService)
+		}
 	}
 
+	if *debug {
+		fmt.Printf("Response with user %s and service %s\n", auth.UserIDString(res.CallerUser), auth.UserIDString(res.CallerService))
+	}
 	return res, nil
 }
